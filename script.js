5dbc214da63cb0869f16d8ad843f11a9865f70fe
Leon Miguel Pengson <miguelpengson@gmail.com>
2024-08-29 20:59:58 +0800

    if the user inputs an invalid input, isError variable

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 let output = document.getElementById('output');
+let isError = false;
