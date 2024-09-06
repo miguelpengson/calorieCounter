131e10bb992cd9b7bef27d9c03eafe01a6908428
Leon Miguel Pengson <miguelpengson@gmail.com>
2024-09-06 18:28:40 +0800

    pass the list of elements to getCaloriesFromInputs function

--- script.js
+++ script.js
@@ -39,6 +39,12 @@ function cleanInputString(str) {
    const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
    const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
    const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+   
+   const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+   const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
  }
 
  function getCaloriesFromInputs(list) {
